240ec938023bbfe539c50af53358b7da4becc5b9
osmantat1 <153751193+osmantat1@users.noreply.github.com>
2024-10-31 14:02:15 +0300

    Update ex12.c

--- ex12.c
+++ ex12.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main() {
+    int number;
+    unsigned long long factorial = 1;
+
+    printf("Enter a number to calculate its factorial (must be less than 50): ");
+    scanf("%d", &number);
+
+    if (number < 0 || number >= 50) {
+        printf(" You entered invalid value");
+    } else {
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+        printf("Factorial of %d is %llu\n", number, factorial);
+    }
+
+    return 0;
+}
