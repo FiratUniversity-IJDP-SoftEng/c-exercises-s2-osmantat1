d5c4680b6a715efaed44e433a90ef0f0799528ba
osmantat1 <153751193+osmantat1@users.noreply.github.com>
2024-10-31 13:52:31 +0300

    Update ex07.c

--- ex07.c
+++ ex07.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+int main(){
+    int i;
+    for(i = 0; i <=10 ; i++){
+        int col1= i*5;
+        int col2 = 100-((i-1)*10+1);
+        int col3= i;
+        
+        printf("%d\t%d\t%d\n",col1,col2,col3);
+    }
+    return 0;
+}
 
